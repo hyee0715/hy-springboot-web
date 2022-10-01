23c768c497624679b03ee152c8cf534f3d5a2663
hyee0715 <hyee0715@naver.com>
2022-10-01 20:01:03 +0900

    Posts 등록 API 구현

---
+++ src/main/java/com/practice/hyspringbootweb/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.practice.hyspringbootweb.service;
+
+import com.practice.hyspringbootweb.domain.posts.PostsRepository;
+import com.practice.hyspringbootweb.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
