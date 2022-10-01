600bdf842fe738c02ea7986e185520ae614683af
hyee0715 <hyee0715@naver.com>
2022-10-01 19:35:15 +0900

    Initial commit

---
+++ src/main/java/com/practice/hyspringbootweb/HySpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.practice.hyspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HySpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HySpringbootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/hyspringbootweb/HySpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.hyspringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HySpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
