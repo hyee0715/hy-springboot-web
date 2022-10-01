1a121eccc1bbea2103414e330c0df20f05dbb9e8
hyee0715 <hyee0715@naver.com>
2022-10-01 20:08:26 +0900

    JPA Auditing으로 생성시간/수정시간 자동화

--- src/main/java/com/practice/hyspringbootweb/HySpringbootWebApplication.java
+++ src/main/java/com/practice/hyspringbootweb/HySpringbootWebApplication.java
@@ -2,7 +2,9 @@ package com.practice.hyspringbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HySpringbootWebApplication {
 
