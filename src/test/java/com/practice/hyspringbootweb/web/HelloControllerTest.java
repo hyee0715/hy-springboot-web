0c49bac703ef2b4105589ef15d2ce240d38dea9c
hyee0715 <hyee0715@naver.com>
2022-10-01 19:47:31 +0900

    Spring Data JPA 테스트 코드 작성

--- src/test/java/com/practice/hyspringbootweb/web/HelloControllerTest.java
+++ src/test/java/com/practice/hyspringbootweb/web/HelloControllerTest.java
@@ -7,12 +7,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers =  HelloController.class)
+@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
