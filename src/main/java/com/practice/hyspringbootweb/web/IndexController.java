a441bb5d4fa24150261517bd98250c254a77c316
hyee0715 <hyee0715@naver.com>
2022-10-02 20:40:58 +0900

    어노테이션 기반으로 코드 개선

--- src/main/java/com/practice/hyspringbootweb/web/IndexController.java
+++ src/main/java/com/practice/hyspringbootweb/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.practice.hyspringbootweb.web;
 
+import com.practice.hyspringbootweb.config.auth.LoginUser;
 import com.practice.hyspringbootweb.config.auth.dto.SessionUser;
 import com.practice.hyspringbootweb.service.PostsService;
 import com.practice.hyspringbootweb.web.dto.PostsResponseDto;
@@ -16,13 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
